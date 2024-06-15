da6e76d26d3ad6e1a6d9d7d96d0e9f6c883a03d5
Rammani Chaudhuri <rchaudhuri.eee18@itbhu.ac.in>
2024-06-15 17:18:33 +0530

    changes

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript/TypeScript/React these days.
+      I've made a few projects in those languages. I also like C++ and Python. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/rammanichaudhuri" target="_blank">Github</a>!`,
+    "cs-degree": `This is my B.Tech degree. I hung it on the wall because I'm proud of it. 
+    I did my B.Tech in Electrical Engineering from IIT BHU, Varanasi and graduated with a CGPA of 8.25.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+    I also make game programming tutorials on YouTube.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@TraversyMedia" target="_blank">TraversyMedia</a> and <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a> (sometimes)!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk. This is where I study and code.
+    Contact me at rammanititli@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit Rammani's portfolio, just close the tab.`,
+  };
